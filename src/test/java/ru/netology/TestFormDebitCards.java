a6a592e507eec1eacc33e127950790cbde94ab72
Svet21 <chuyak.svetlana@gmail.com>
2021-10-07 23:13:17 +0500

    Test started selenium

--- src/test/java/ru/netology/TestFormDebitCards.java
+++ src/test/java/ru/netology/TestFormDebitCards.java
@@ -18,7 +18,7 @@ public class TestFormDebitCards {
     private WebDriver driver;
     @BeforeAll
     static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
     }
 
     @BeforeEach
@@ -45,19 +45,21 @@ public class TestFormDebitCards {
         elements.get(1).sendKeys("+79270000000");
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.className("button")).click();
-        String text = driver.findElement(By.className("alert-success")).getText();
-        assertEquals("Ваша заявка успешно отправлена!", text.trim());
+        String text = driver.findElement(By.className("paragraph_theme_alfa-on-white")).getText();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        assertEquals(expected, text.trim());
     }
 
     @Test
     void shouldTestV2() {
         driver.get("http://localhost:9999");
-        WebElement form = driver.findElement(By.cssSelector("[data-test-id=callback-form]"));
-        form.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Василий");
-        form.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79270000000");
-        form.findElement(By.cssSelector("[data-test-id=agreement]")).click();
-        form.findElement(By.cssSelector("[data-test-id=submit]")).click();
-        String text = driver.findElement(By.className("alert-success")).getText();
-        assertEquals("Ваша заявка успешно отправлена!", text.trim());
+        //WebElement form = driver.findElement(By.cssSelector("[data-test-id=callback-form]"));
+        driver.findElement(By.cssSelector("[type='text']")).sendKeys("Василий");
+        driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+79270000000");
+        driver.findElement(By.cssSelector(".checkbox__box")).click();
+        driver.findElement(By.cssSelector("button")).click();
+        String text = driver.findElement(By.className("paragraph_theme_alfa-on-white")).getText();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        assertEquals(expected, text.trim());
     }
 }
