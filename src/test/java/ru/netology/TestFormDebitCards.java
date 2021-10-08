f3c5697fbda44727d0bb893ccefa08d5d13dbb1a
Svet21 <chuyak.svetlana@gmail.com>
2021-10-08 11:29:18 +0500

    Add tests

--- src/test/java/ru/netology/TestFormDebitCards.java
+++ src/test/java/ru/netology/TestFormDebitCards.java
@@ -41,7 +41,7 @@ public class TestFormDebitCards {
     void shouldTestV1() {
         driver.get("http://localhost:9999");
         List<WebElement> elements = driver.findElements(By.className("input__control"));
-        elements.get(0).sendKeys("Василий");
+        elements.get(0).sendKeys("Петров Василий");
         elements.get(1).sendKeys("+79270000000");
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.className("button")).click();
@@ -54,7 +54,7 @@ public class TestFormDebitCards {
     void shouldTestV2() {
         driver.get("http://localhost:9999");
         //WebElement form = driver.findElement(By.cssSelector("[data-test-id=callback-form]"));
-        driver.findElement(By.cssSelector("[type='text']")).sendKeys("Василий");
+        driver.findElement(By.cssSelector("[type='text']")).sendKeys("Петров Василий");
         driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+79270000000");
         driver.findElement(By.cssSelector(".checkbox__box")).click();
         driver.findElement(By.cssSelector("button")).click();
@@ -62,4 +62,75 @@ public class TestFormDebitCards {
         String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
         assertEquals(expected, text.trim());
     }
+    @Test
+    void shouldTestFieldNameEmpty() {
+        driver.get("http://localhost:9999");
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("");
+        elements.get(1).sendKeys("+79270000000");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(By.className("input__sub")).getText();
+        String expected = "Поле обязательно для заполнения";
+        assertEquals(expected, text.trim());
+    }
+    @Test
+    void shouldTestFieldNameWrong() {
+        driver.get("http://localhost:9999");
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Petrov Vasili");
+        elements.get(1).sendKeys("+79270000000");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(By.className("input__sub")).getText();
+        String expected = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
+        assertEquals(expected, text.trim());
+    }
+    @Test
+    void shouldTestFieldTelWrong() {
+        driver.get("http://localhost:9999");
+        //WebElement form = driver.findElement(By.cssSelector("[data-test-id=callback-form]"));
+        driver.findElement(By.cssSelector("[type='text']")).sendKeys("Петров Василий");
+        driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+7927000000");
+        driver.findElement(By.cssSelector(".checkbox__box")).click();
+        driver.findElement(By.cssSelector("button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id=phone] .input__sub")).getText();
+        String expected = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
+        assertEquals(expected, text.trim());
+    }
+    @Test
+    void shouldTestFieldTelEmpty() {
+        driver.get("http://localhost:9999");
+        //WebElement form = driver.findElement(By.cssSelector("[data-test-id=callback-form]"));
+        driver.findElement(By.cssSelector("[type='text']")).sendKeys("Петров Василий");
+        driver.findElement(By.cssSelector("[type='tel']")).sendKeys("");
+        driver.findElement(By.cssSelector(".checkbox__box")).click();
+        driver.findElement(By.cssSelector("button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id=phone] .input__sub")).getText();
+        String expected = "Поле обязательно для заполнения";
+        assertEquals(expected, text.trim());
+    }
+    @Test
+    void shouldTestFieldTelWithoutPlus() {
+        driver.get("http://localhost:9999");
+        //WebElement form = driver.findElement(By.cssSelector("[data-test-id=callback-form]"));
+        driver.findElement(By.cssSelector("[type='text']")).sendKeys("Петров Василий");
+        driver.findElement(By.cssSelector("[type='tel']")).sendKeys("89270000000");
+        driver.findElement(By.cssSelector(".checkbox__box")).click();
+        driver.findElement(By.cssSelector("button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id=phone] .input__sub")).getText();
+        String expected = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
+        assertEquals(expected, text.trim());
+    }
+    @Test
+    void shouldTestFieldTelWithoutCheckBox() {
+        driver.get("http://localhost:9999");
+        //WebElement form = driver.findElement(By.cssSelector("[data-test-id=callback-form]"));
+        driver.findElement(By.cssSelector("[type='text']")).sendKeys("Петров Василий");
+        driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+79270000000");
+        //driver.findElement(By.cssSelector(".checkbox__box")).click();
+        driver.findElement(By.cssSelector("button")).click();
+        driver.findElement(By.cssSelector(".input_invalid"));
+    }
+
 }
